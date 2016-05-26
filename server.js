65df773b8297a17ad4d588bfa0ea2c739dcce0af
abdullah2891 <abdullah2891@gmail.com>
2016-05-26 19:00:20 +0600

    authController added

--- server.js
+++ server.js
@@ -2,19 +2,26 @@ var express = require('express');
 var app =express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
+var expressSession = require('express-session');
+var passport = require('passport');
 //db
 mongoose.connect("mongodb://localhost/newPoll");
 //mongoose.connect("mongodb://heroku_c3c0n4nt:rgj9aa3tjfeketnkg434mb1dnm@ds011863.mlab.com:11863/heroku_c3c0n4nt");
 //controller
 pollController = require("./controller/polls");
-
+authController = require('./controller/auth');
 
 //middlewares
 app.use(bodyParser.urlencoded({exteded:true}));
 app.use(bodyParser.json());
 var router = express.Router();
 app.use('/api',router);
+app.use(expressSession({
+secret:"this is a secret"
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 //routes
 
 router.route('/poll')
@@ -29,6 +36,14 @@ router.route('/vote/cast')
 
 router.route('/vote/:count')
 .get(pollController.countVote);
+
+app.get('/login/twitter/callback',
+authController.authenticate,
+function(req,res){
+  console.log("LOGGED IN");
+  console.log(req.session["_passport"]);
+  res.redirect('/');
+})
 
 app.use('/',express.static(__dirname+'/client'));
 
