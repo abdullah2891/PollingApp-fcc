1a37625703607fab821790accd7a362b257913d1
abdullah2891 <abdullah2891@gmail.com>
2016-05-29 21:00:44 +0600

    real time vote update

--- client/app.js
+++ client/app.js
@@ -47,7 +47,9 @@ weatherApp.controller('homeController', ['$scope','$http', function($scope,$http
       })
           .success(function(result){
           $scope.status = result;
-        
+          $http.get('/api/poll').then(function(response){
+              $scope.data = response.data;
+            })
 
       })
            .error(function(err){
