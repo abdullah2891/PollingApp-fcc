a4a949f537f1e08bacb6aee7f554f9d2789d249e
Abdullah Rahman <abdullah2891@gmail.com>
2019-07-13 18:23:52 -0400

    removed config folder

--- config/twitter.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  key:"8i012pQfHOX3XDHz3surESXlz",
-  secret:"HckTjwCVdgfP5OKtFhAcAD4rY9fZgqMHIxVY5HuLXGFg2j6fm5",
-  redirect_url: "http://127.0.0.1:3001/login/twitter/callback", 
-}
